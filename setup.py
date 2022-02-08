287b55e03a85f03596e3583c19ce5bbc0ba10bb3
Damian Barabonkov <damianb@alum.mit.edu>
2022-02-09 00:36:20 +0100

    Prepared 'setup.py' for pypi repository

--- setup.py
+++ setup.py
@@ -2,16 +2,31 @@ from setuptools import setup
 
 setup(
     name="algopytest",
-    version="0.1.0",
+    version="1.0.0",
     description="A pytest plugin to write Algorand smart contract test suites more easily",
     url="https://github.com/DamianB-BitFlipper/algopytest.git",
     author="Damian Barabonkov",
     author_email="damianb@alum.mit.edu",
     license="MIT",
     packages=["algopytest"],
-    install_requires=["pytest"],
+    keywords=["Algorand", "Smart Contract", "Pytest", "Unit Tests", "Test Suite"],
+    download_url="https://github.com/DamianB-BitFlipper/algopytest/archive/refs/tags/v1.0.0.tar.gz",
+    install_requires=[
+        "pytest",
+        "py-algorand-sdk",
+        "pyteal",
+    ],
     # This makes this plugin available to pytest
     entry_points={"pytest11": ["name_of_plugin = algopytest.fixtures"]},
     # Custom PyPI classifier for pytest plugins
-    classifiers=["Framework :: Pytest"],
+    classifiers=[
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
