17a4d2f239a64d90b97f365b40d527319c233255
Damian Barabonkov <damianb@alum.mit.edu>
2022-08-15 19:28:00 +0200

    Created a Context Manger class for 'deploy_smart_contract' to automatically clean up the smart contract after use

--- algopytest/smart_program_ops.py
+++ algopytest/smart_program_ops.py
@@ -9,6 +9,18 @@ from .transaction_ops import create_app, delete_app
 from .type_stubs import PyTEAL
 
 
+class DeployedSmartContract:
+    def __init__(self, owner: AlgoUser, app_id: int):
+        self.owner = owner
+        self.app_id = app_id
+
+    def __enter__(self):
+        return self.app_id
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        delete_app(self.owner, self.app_id)
+
+
 def deploy_smart_contract(
     owner: AlgoUser,
     approval_program: PyTEAL,
@@ -18,7 +30,7 @@ def deploy_smart_contract(
     local_bytes: int = 0,
     global_ints: int = 0,
     global_bytes: int = 0,
-) -> Tuple[int, Callable]:
+) -> DeployedSmartContract:
     approval_compiled = compile_program(approval_program, Mode.Application, version)
     clear_compiled = compile_program(clear_program, Mode.Application, version)
     global_schema = transaction.StateSchema(global_ints, global_bytes)
@@ -33,7 +45,4 @@ def deploy_smart_contract(
         local_schema,
     )
 
-    def clean_up_fn() -> None:
-        delete_app(owner, app_id)
-
-    return app_id, clean_up_fn
+    return DeployedSmartContract(owner, app_id)
