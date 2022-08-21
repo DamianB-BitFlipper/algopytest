d7a6a0a18d10ff0bd1abb22854c965884886877c
Damian Barabonkov <damianb@alum.mit.edu>
2022-08-21 21:08:32 +0200

    Commented out development code from '.spellcheck.sh' script

--- .spellcheck.sh
+++ .spellcheck.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Spellchecker docs: https://github.com/myint/scspell
-if [ $1 == "interactive" ]; then
-    scspell $@ --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd) --report-only
-else
-    scspell $2 --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd)
-fi
+scspell $@ --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd) --report-only
+
+# TODO! Make this work
+#if [ $1 == "interactive" ]; then
+#    scspell $@ --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd) --report-only
+#else
+#    scspell $2 --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd)
+#fi
