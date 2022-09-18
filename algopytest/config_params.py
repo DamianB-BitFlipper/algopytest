7caa3ae749806b4b27bee26140bed5edd2144942
Damian Barabonkov <damianb@alum.mit.edu>
2022-09-18 17:37:03 +0200

    Utlize the KMD to get the private key of sandbox wallet accounts

--- algopytest/config_params.py
+++ algopytest/config_params.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 from typing import Optional
 
 
@@ -10,11 +9,14 @@ class _ConfigParams:
     algod_token: str = (
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     )
+
     indexer_address: str = "http://localhost:8980"
     indexer_token: str = ""
 
-    # Assume that the sandbox is the sibling directory of this project
-    sandbox_dir: Path = Path(".").resolve().parent / "sandbox"
+    kmd_address: str = "http://localhost:4002"
+    kmd_token: str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    kmd_wallet_name: str = "unencrypted-default-wallet"
+    kmd_wallet_password: str = ""
 
     initial_funds_account: Optional[str] = None
 
@@ -27,15 +29,15 @@ class _ConfigParams:
         self.algod_token = os.environ.get("ALGOD_TOKEN") or self.algod_token
         self.indexer_address = os.environ.get("INDEXER_ADDRESS") or self.indexer_address
         self.indexer_token = os.environ.get("INDEXER_TOKEN") or self.indexer_token
-
-        # Convert the `SANDBOX_DIR` to a `pathlib.Path` if it exists
-        env_sandbox_dir = os.environ.get("SANDBOX_DIR")
-        if env_sandbox_dir is not None:
-            self.sandbox_dir = Path(env_sandbox_dir)
-
-        env_initial_funds_account = os.environ.get("INITIAL_FUNDS_ACCOUNT")
-        if env_initial_funds_account is not None:
-            self.initial_funds_account = env_initial_funds_account
+        self.kmd_address = os.environ.get("KMD_ADDRESS") or self.kmd_address
+        self.kmd_token = os.environ.get("KMD_TOKEN") or self.kmd_token
+        self.kmd_wallet_name = os.environ.get("KMD_WALLET_NAME") or self.kmd_wallet_name
+        self.kmd_wallet_password = (
+            os.environ.get("KMD_WALLET_PASSWORD") or self.kmd_wallet_password
+        )
+        self.initial_funds_account = (
+            os.environ.get("INITIAL_FUNDS_ACCOUNT") or self.initial_funds_account
+        )
 
         # Convert the `INDEXER_TIMEOUT` to an `int` if it exists
         env_indexer_timeout = os.environ.get("INDEXER_TIMEOUT")
