2d769ce838339d345d25cebdec86cc0d50d5c0ae
Damian Barabonkov <damianb@alum.mit.edu>
2022-11-19 16:34:26 +0100

    Made the `create_app` into a context manager (#62)
    * The create_app using an int subclassed context manager
    
    * Converted demo applications to use context managed 'create_app'
    
    * Added to CHANGELOG
    
    * Updated submodules with 'create_app' changes

--- algopytest/transaction_ops.py
+++ algopytest/transaction_ops.py
@@ -3,11 +3,19 @@ from __future__ import annotations
 import logging
 from functools import wraps
 from types import TracebackType
-from typing import Any, Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, List, Optional, Tuple, Type, Union
 
+import pyteal
+import typing_extensions
 from algosdk.future import transaction as algosdk_transaction
+from pyteal import Mode
 
-from .client_ops import pending_transaction_info, process_transactions, suggested_params
+from .client_ops import (
+    compile_program,
+    pending_transaction_info,
+    process_transactions,
+    suggested_params,
+)
 from .entities import AlgoUser, MultisigAccount, _NullUser
 from .type_stubs import P, TransactionT
 
@@ -57,6 +65,29 @@ class TxnIDContext:
 
         # Disable any global modifiers
         _with_txn_id = None
+
+
+class DeployedAppID(int):
+    """Subclass the `int` so that it can be used as a context manager or directly."""
+
+    owner: AlgoUser
+
+    def __new__(cls, app_id: int, owner: AlgoUser):
+        _app_id = int.__new__(cls, app_id)
+        _app_id.owner = owner
+        return _app_id
+
+    def __enter__(self) -> int:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> typing_extensions.Literal[False]:
+        delete_app(self.owner, self)
+        return False
 
 
 def transaction_boilerplate(
@@ -145,12 +176,93 @@ def transaction_boilerplate(
     return decorator
 
 
+def create_app(
+    owner: AlgoUser,
+    approval_program: pyteal.Expr,
+    clear_program: pyteal.Expr,
+    version: int = 5,
+    local_ints: int = 0,
+    local_bytes: int = 0,
+    global_ints: int = 0,
+    global_bytes: int = 0,
+    *,
+    app_args: Optional[List[Union[str, int]]] = None,
+    accounts: Optional[List[AlgoUser]] = None,
+    foreign_apps: Optional[List[int]] = None,
+    foreign_assets: Optional[List[int]] = None,
+    note: str = "",
+    lease: str = "",
+    rekey_to: Optional[AlgoUser] = None,
+    extra_pages: int = 0,
+) -> int:
+    # Compile the smart contract
+    approval_compiled = compile_program(approval_program, Mode.Application, version)
+    clear_compiled = compile_program(clear_program, Mode.Application, version)
+    global_schema = algosdk_transaction.StateSchema(global_ints, global_bytes)
+    local_schema = algosdk_transaction.StateSchema(local_ints, local_bytes)
+
+    # Deploy the compiled smart contract
+    return create_compiled_app(
+        owner,
+        approval_compiled,
+        clear_compiled,
+        global_schema,
+        local_schema,
+        app_args=app_args,
+        accounts=accounts,
+        foreign_apps=foreign_apps,
+        foreign_assets=foreign_assets,
+        note=note,
+        lease=lease,
+        rekey_to=rekey_to,
+        extra_pages=extra_pages,
+    )
+
+
+def create_compiled_app(
+    owner: AlgoUser,
+    approval_compiled: bytes,
+    clear_compiled: bytes,
+    global_schema: algosdk_transaction.StateSchema,
+    local_schema: algosdk_transaction.StateSchema,
+    *,
+    params: Optional[algosdk_transaction.SuggestedParams] = None,
+    app_args: Optional[List[Union[str, int]]] = None,
+    accounts: Optional[List[AlgoUser]] = None,
+    foreign_apps: Optional[List[int]] = None,
+    foreign_assets: Optional[List[int]] = None,
+    note: str = "",
+    lease: str = "",
+    rekey_to: Optional[AlgoUser] = None,
+    extra_pages: int = 0,
+) -> int:
+    # Deploy the smart contract
+    app_id = _create_compiled_app(
+        owner,
+        approval_compiled,
+        clear_compiled,
+        global_schema,
+        local_schema,
+        app_args=app_args,
+        accounts=accounts,
+        foreign_apps=foreign_apps,
+        foreign_assets=foreign_assets,
+        note=note,
+        lease=lease,
+        rekey_to=rekey_to,
+        extra_pages=extra_pages,
+    )
+
+    # Return a int sub-classed context manager
+    return DeployedAppID(app_id, owner)
+
+
 # The return type is `int` modified by `return_fn`
 @transaction_boilerplate(
     format_finish=lambda txninfo: f'app-id={txninfo["application-index"]}',
     return_fn=lambda txninfo: txninfo["application-index"],
 )
-def create_app(
+def _create_compiled_app(
     owner: AlgoUser,
     approval_compiled: bytes,
     clear_compiled: bytes,
