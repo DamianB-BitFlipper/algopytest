6d61cc694bc31c2776d0baaea5267239add76ff3
Damian Barabonkov <damianb@alum.mit.edu>
2023-02-03 22:57:15 +0100

    Transitioned project to use crate-ci/typos for source code spell-checking

--- .spellcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Spellchecker docs: https://github.com/myint/scspell
-if [ $1 != "interactive" ]; then
-    scspell $@ --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd) --report-only
-else
-    scspell $2 --use-builtin-base-dict --override-dictionary ./.dict --relative-to $(pwd)
-fi
